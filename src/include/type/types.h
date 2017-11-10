37b1f61b6615c79032b648b5c7c9955ac55cdf68
Allison Wang <allisonwang520@gmail.com>
2017-11-10 14:15:07 -0500

    add like impl

--- src/include/type/types.h
+++ src/include/type/types.h
@@ -1048,7 +1048,7 @@ enum class OperatorId : uint32_t {
   Floor,
 
   // Add more operators here, before the last "Invalid" entry
-  like,
+  Like,
   Invalid
 };
 std::string OperatorIdToString(OperatorId op_id);
